025d37b7c1f7b91c77df6f899228c7ec2655a64e
Tom Elgersma <tom.elgersma@hotmail.com>
2018-12-17 14:27:52 +0100

    removed folders

--- 2D Platformer Xmas/Assets/SandBox/Tom/PlayerController.cs
+++ 2D Platformer Xmas/Assets/SandBox/Tom/PlayerController.cs
@@ -7,22 +7,21 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
     public GameObject ps;
-    private SpriteRenderer spriteRenderer;
-    private Animator animator;
+    //private SpriteRenderer spriteRenderer;
 
     // Use this for initialization
     void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        //spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     protected void ComputeVelocity()
     {
-        Vector2 move = Vector2.zero;
+        //Vector2 move = Vector2.zero;
 
-        move.x = Input.GetAxis("Horizontal");
+        //move.x = Input.GetAxis("Horizontal");
 
-        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < -0.01f));
+        //bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < -0.01f));
         /*
         if (move.x > 0.01f || move.x < -0.01f)
         {
@@ -43,13 +42,13 @@ public class PlayerController : MonoBehaviour
             turn.transform.rotation = Quaternion.Euler(0, -90, 0);
         }
         */
-        if (flipSprite)
-        {
-            spriteRenderer.flipX = !spriteRenderer.flipX;
-        }
+        //if (flipSprite)
+        //{
+        //    spriteRenderer.flipX = !spriteRenderer.flipX;
+        //}
         //animator.SetBool("grounded", grounded);
         //animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
 
-        targetVelocity = move * maxSpeed;
+        //targetVelocity = move * maxSpeed;
     }
 }
