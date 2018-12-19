c4f1c530df35a111b5cd506f693b460ae25ff1af
Timur <Timur.d.sel074@gmail.com>
2018-12-19 14:38:27 +0100

    *map changes

---
+++ 2D Platformer Xmas/Assets/SandBox/Scripts/Collectibles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectibles : MonoBehaviour {
+
+    public static Collectibles instace;
+    public int specialAttackMeter = 0; 
+
+    void Start()
+    {
+        instace = this;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "collectible")
+        {
+            Destroy(other.gameObject);
+            specialAttackMeter++;
+        }
+    }
+}
---
+++ 2D Platformer Xmas/Assets/SandBox/Scripts/Enemys/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public bool flipped;
+
+    public Vector3 currRot;
+
+    Transform enemyTrans;
+
+    public Rigidbody2D enemyBody;
+
+    private void Start()
+    {
+        enemyTrans = this.transform;
+        enemyBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 enemyVel = enemyBody.velocity;
+        enemyVel.x = -enemyTrans.right.x * speed;
+        enemyBody.velocity = enemyVel;
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "TurnPoint")
+        {
+            currRot = enemyTrans.eulerAngles;
+            currRot.y += 180;
+            enemyTrans.eulerAngles = currRot;
+        }
+    }
+}
