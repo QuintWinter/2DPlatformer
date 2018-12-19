72b003f419a096d1f6eb5ee11f9b01dde4663f41
sato <robinleenheer13@gmail.com>
2018-12-19 12:09:19 +0100

    sound

---
+++ 2D Platformer Xmas/Assets/SandBox/Robin/Scripts/SoundManagerPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerPlayer : MonoBehaviour {
+
+    AudioSource source;
+    public AudioClip Jump, Hit, Die, Woosh, Boom, Pickup;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlayHit()
+    {
+        source.PlayOneShot(Hit);
+    }
+
+    public void PlayJump()
+    {
+        source.PlayOneShot(Jump);
+    }
+
+    public void PlayDie()
+    {
+        source.PlayOneShot(Die);
+    }
+
+    public void PlayWoosh()
+    {
+        source.PlayOneShot(Woosh);
+    }
+
+    public void PlayBoom()
+    {
+        source.PlayOneShot(Boom);
+    }
+
+    public void PlayPickup()
+    {
+        source.PlayOneShot(Pickup);
+    }
+}
