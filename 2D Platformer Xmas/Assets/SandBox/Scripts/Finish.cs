f9953c42eed71bcb616477012e9e4cd8e1992b00
Timur <Timur.d.sel074@gmail.com>
2018-12-20 09:35:09 +0100

    +finish

---
+++ 2D Platformer Xmas/Assets/SandBox/Scripts/Finish.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour {
+
+    GameObject player;
+
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider other)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
